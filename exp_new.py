0cb620051379d738c585cae1c982809c31b80337
Naveen8297 <65307153+Naveen8297@users.noreply.github.com>
2021-10-06 04:13:27 +0200

    Update exp_new.py

--- exp_new.py
+++ exp_new.py
@@ -21,7 +21,7 @@ names = [
         "multipeak",
     ]
 names += ["sphere", "doublelinearslope"]  #, "stepdoublelinearslope"]
-names = ["cigar", "altcigar", "ellipsoid", "altellipsoid", "stepellipsoid", "discus", "bentcigar"]
+names += ["cigar", "altcigar", "ellipsoid", "altellipsoid", "stepellipsoid", "discus", "bentcigar"]
 names += ["deceptiveillcond", "deceptivepath"]   #list of objective functions
 
 #algorithms = ["ECMA"] #["CMandAS2", "ECMA", "Cobyla", "MetaModel", "CMA", "DiagonalCMA", "FCMA", "BOBYQA", "F127CMA", "Powell", "SQP"]
@@ -36,8 +36,8 @@ def param(dimension):
 
 def main():
 	for name in names:
-		ioh.problem.wrap_real_problem(lambda x: getattr(functions.corefuncs, name)(np.array(x)), name+'_ng', optimization_type=ioh.OptimizationType.Minimization, n_variables = dimension)
-		f = ioh.get_problem(name+'_ng', 0, dim=dimension)
+		f = ioh.problem.wrap_real_problem(lambda x: getattr(functions.corefuncs, name)(np.array(x)), name+'_ng', optimization_type=ioh.OptimizationType.Minimization, n_variables = dimension)
+		#f = ioh.get_problem(name+'_ng', 0, dim=dimension)
 		for runs in range(10):
 			l = logger.Analyzer(root=r"data_new/data_"+name, folder_name="run")
 			f.attach_logger(l)
